e697e0a6ae0d1ecb8ae74898ad3b0ea5ebe2e30f
willy <shen.willy95@gmail.com>
2021-06-28 13:46:38 +0700

    add gas reporter & kovan conf

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,12 +7,14 @@ import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-web3";
+import "hardhat-gas-reporter";
 import "solidity-coverage";
 
 const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
 const RINKEBY_PRIVATE_KEY =
   process.env.RINKEBY_PRIVATE_KEY! ||
   "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"; // well known private key
+const KOVAN_PRIVATE_KEY = process.env.KOVAN_PRIVATE_KEY!;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 const config: HardhatUserConfig = {
@@ -36,6 +38,10 @@ const config: HardhatUserConfig = {
       url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
       accounts: [RINKEBY_PRIVATE_KEY],
     },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [KOVAN_PRIVATE_KEY],
+    },
     coverage: {
       url: "http://127.0.0.1:8555", // Coverage launches its own ganache-cli client
     },
@@ -45,6 +51,12 @@ const config: HardhatUserConfig = {
     // Obtain one at https://etherscan.io/
     apiKey: ETHERSCAN_API_KEY,
   },
+  gasReporter: {
+    enabled: false,
+    currency: 'USD',
+    coinmarketcap: '7e5a9ae8-93ff-469a-b5cd-3252d09371a8',
+    gasPrice: 20
+  }
 };
 
 export default config;

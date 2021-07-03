a96b7845f3ad63bc4bf26e2f651ac009100b88e0
willy <shen.willy95@gmail.com>
2021-07-03 21:57:51 +0700

    update graph

--- graph/ubiroll/src/mapping.ts
+++ graph/ubiroll/src/mapping.ts
@@ -1,63 +1,22 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  Contract,
+  Ubiroll,
   BetCreated,
   BetFinalized,
   OwnershipTransferred
-} from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+} from "../generated/Ubiroll/Ubiroll"
+import { Bet } from "../generated/schema"
 
 export function handleBetCreated(event: BetCreated): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let betID = event.params.id;
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.id = event.params.id
-  entity.player = event.params.player
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.bets(...)
-  // - contract.calculatePrize(...)
-  // - contract.gamePaused(...)
-  // - contract.houseEdge(...)
-  // - contract.maxPrize(...)
-  // - contract.minBet(...)
-  // - contract.oracle(...)
-  // - contract.oracleRequestToBet(...)
-  // - contract.owner(...)
-  // - contract.vault(...)
+	let contract = Ubiroll.bind(event.address);
+	// return contract.disputes(disputeID)
+  
 }
 
-export function handleBetFinalized(event: BetFinalized): void {}
+export function handleBetFinalized(event: BetFinalized): void {
+  
+}
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {}

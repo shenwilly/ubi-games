dd12b850aec4e40c637d3ec8d7e9c43c187e85a5
willy <shen.willy95@gmail.com>
2021-07-03 22:04:55 +0700

    update mapping

--- graph/ubiroll/src/mapping.ts
+++ graph/ubiroll/src/mapping.ts
@@ -1,6 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  Ubiroll,
   BetCreated,
   BetFinalized,
   OwnershipTransferred
@@ -8,15 +6,28 @@ import {
 import { Bet } from "../generated/schema"
 
 export function handleBetCreated(event: BetCreated): void {
-  let betID = event.params.id;
+  let betID = event.params.id.toString();
 
-	let contract = Ubiroll.bind(event.address);
-	// return contract.disputes(disputeID)
-  
+  let bet = Bet.load(betID);
+	if (bet == null) {
+		bet = new Bet(betID)
+    bet.player = event.params.player;
+    bet.chance = event.params.chance;
+    bet.amount = event.params.amount;
+    bet.requestId = event.params.requestId;
+    
+		bet.save()
+	} 
 }
 
 export function handleBetFinalized(event: BetFinalized): void {
-  
+  let betID = event.params.id.toString();
+
+  let bet = Bet.load(betID);
+  bet.result = event.params.result;
+  bet.win = event.params.win;
+    
+  bet.save();
 }
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {}

7fb84d9f63865071c8122871f6b20a5b2a8cf97d
willy <shen.willy95@gmail.com>
2021-07-04 11:32:26 +0700

    add finished & prize subgraph field

--- graph/ubiroll/src/mapping.ts
+++ graph/ubiroll/src/mapping.ts
@@ -1,4 +1,5 @@
 import {
+  Ubiroll,
   BetCreated,
   BetFinalized,
   OwnershipTransferred
@@ -15,6 +16,12 @@ export function handleBetCreated(event: BetCreated): void {
     bet.chance = event.params.chance;
     bet.amount = event.params.amount;
     bet.requestId = event.params.requestId;
+    bet.timestamp = event.block.timestamp;
+
+    let ubiroll = Ubiroll.bind(event.address);
+    let betObj = ubiroll.bets(event.params.id);
+    bet.prize = betObj.value5;
+    bet.finished = false;
     
 		bet.save()
 	} 
@@ -26,6 +33,7 @@ export function handleBetFinalized(event: BetFinalized): void {
   let bet = Bet.load(betID);
   bet.result = event.params.result;
   bet.win = event.params.win;
+  bet.finished = true;
     
   bet.save();
 }
